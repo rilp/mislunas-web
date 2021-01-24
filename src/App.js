3c4c7cd48760fab9ec7a36e80ab62698dd670502
Victor <victor@Mufaza.local>
2021-01-23 22:02:04 -0600

    Header, contact, footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import logo from './logo.svg'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
 
 class Header extends React.Component {
   render() {
@@ -12,10 +11,25 @@ class Header extends React.Component {
         <Container >
           <Row>
               <img src={logo} className="logo" alt="Mis Lunas" />
-              <hr></hr>
+              <hr/>
           </Row>
         </Container>
       </header>
+    )
+  }
+}
+
+class ContactImage extends React.Component {
+  render() {
+    const img = this.props.img;
+    const name = this.props.name;
+    const info = this.props.info;
+    return (
+      <Row className={name}>
+        <a href={info}>
+          <img src={img} className={name} alt={name}/>
+        </a>
+      </Row>
     )
   }
 }
@@ -25,16 +39,28 @@ class Contact extends React.Component {
     return (
       <div id="contact">
         <h2>Contacto</h2>
+        <hr/>
+        <Container className="contactCard">
+          <ContactImage name={"email"} img={"./img/mail.svg"} info={"mailto:zayra.zambrano@gmail.com"}/>
+          <ContactImage name={"facebook"} img={"./img/facebook.svg"} info={"https://www.facebook.com/zay.zambrano"}/>
+          <ContactImage name={"whatsapp"} img={"./img/whatsapp.svg"} info={"https://wa.link/if9emg"}/>
+        </Container>
       </div>
     )
   }
 }
+
 
 function App() {
   return (
     <div>
       <Header/>
       <Contact/>
+      <Container>
+        <Row id="copyright">
+          <span>Desarrolado y dise&ntilde;o por <a href="mailto:victor.baruch@gmail.com">Vic Baruch</a>. &copy; 2021. Todos los derechos reservados</span>
+        </Row>
+      </Container>
     </div>
   );
 }
